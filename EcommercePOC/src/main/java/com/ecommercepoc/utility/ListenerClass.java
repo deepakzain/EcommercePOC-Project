444e25b55e77a5707dd5d9836f3f7c5368ac77cf
deepak.j <deepak.j@TL353.Technovert.in>
2023-05-17 12:36:20 +0530

    Commit new changes

--- EcommercePOC/src/main/java/com/ecommercepoc/utility/ListenerClass.java
+++ EcommercePOC/src/main/java/com/ecommercepoc/utility/ListenerClass.java
@@ -35,13 +35,12 @@ public class ListenerClass extends ExtentManager implements ITestListener {
 	public void onTestFailure(ITestResult result) {
 		if (result.getStatus() == ITestResult.FAILURE) {
 			try {
-				test.log(Status.FAIL,
-						MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
-				test.log(Status.FAIL,
-						MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
+				test.log(Status.FAIL,MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
+				test.log(Status.FAIL,MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
+				
 				String imgPath = action.screenShot(BaseClass.getDriver(), result.getName());
-			
 				test.fail("ScreenShot is Attached", MediaEntityBuilder.createScreenCaptureFromPath(imgPath).build());
+				test.addScreenCaptureFromPath(imgPath);
 				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
