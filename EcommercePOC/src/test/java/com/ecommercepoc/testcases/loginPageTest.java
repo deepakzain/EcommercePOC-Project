50bd2a0a993745f1ff03f7f7f417a0d0349c79bc
deepak.j <deepak.j@TL353.Technovert.in>
2023-05-17 16:23:40 +0530

    Added exclude methods in testng xml

--- EcommercePOC/src/test/java/com/ecommercepoc/testcases/loginPageTest.java
+++ EcommercePOC/src/test/java/com/ecommercepoc/testcases/loginPageTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 //import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
 
 import com.ecommercepoc.base.BaseClass;
 import com.ecommercepoc.dataprovider.DataProviders;
